55318bbab03add60079a886a4983884d1515ff72
AmalH <hichri.amal@gmail.com>
2017-11-11 14:44:49 +0100

    testing branching _ added iKotlin_Amal branch

--- app/src/main/java/com/androidprojects/esprit/ikotlin/activities/LoginActivity.java
+++ app/src/main/java/com/androidprojects/esprit/ikotlin/activities/LoginActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.androidprojects.esprit.ikotlin.R;
 
@@ -16,10 +15,9 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        /** disabling actionBar **/
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        ((Button) findViewById(R.id.confirmLogin_btn)).setOnClickListener(new View.OnClickListener() {
+        /*** disabling actionBar ****/
+        getSupportActionBar().hide();
+        findViewById(R.id.confirmLogin_btn).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(getApplicationContext(),HomeActivity.class));
