30c9094d44ea97217ca0badfc1a087d7d70c19e8
AmalH <hichri.amal@gmail.com>
2017-11-14 14:20:27 +0100

    gradleEdits

--- app/src/main/java/com/androidprojects/esprit/ikotlin/activities/ProfileActivity.java
+++ app/src/main/java/com/androidprojects/esprit/ikotlin/activities/ProfileActivity.java
@@ -5,10 +5,18 @@ import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 import com.androidprojects.esprit.ikotlin.R;
 import com.androidprojects.esprit.ikotlin.adapters.ProfileTabs_Adapter;
+import com.androidprojects.esprit.ikotlin.models.User;
+import com.linkedin.platform.APIHelper;
+import com.linkedin.platform.errors.LIApiError;
+import com.linkedin.platform.listeners.ApiListener;
+import com.linkedin.platform.listeners.ApiResponse;
+
+import org.json.JSONObject;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -36,6 +44,26 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                     startActivity(new Intent(getApplicationContext(),ProfileSettingsActivity.class));
+            }
+        });
+
+
+        /*** testing linkedin API ***/
+        String url = "https://api.linkedin.com/v1/people/~:(id,first-name,last-name,picture-url)";
+
+        final APIHelper apiHelper = APIHelper.getInstance(getApplicationContext());
+        User user;
+        apiHelper.getRequest(this, url, new ApiListener() {
+            @Override
+            public void onApiSuccess(ApiResponse apiResponse) {
+                Log.d("LINKEDIN API",apiResponse.getResponseDataAsJson().toString());
+                JSONObject O = apiResponse.getResponseDataAsJson();
+
+            }
+
+            @Override
+            public void onApiError(LIApiError liApiError) {
+                Log.d("LINKEDIN API","failure");
             }
         });
     }
