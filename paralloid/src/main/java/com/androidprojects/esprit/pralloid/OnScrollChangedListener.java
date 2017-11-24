093ea42374fb12ef59142806460ba9358040bba7
Amal Hichri <hichri.amal@hotmail.fr>
2017-11-24 17:11:54 +0100

    ++ turned back the local dependency on paralloid project ( until I fix the remote one ) .

---
+++ paralloid/src/main/java/com/androidprojects/esprit/pralloid/OnScrollChangedListener.java
@@ -0,0 +1,8 @@
+package com.androidprojects.esprit.pralloid;
+
+/**
+ * OnScrolled changed Listener for {@link android.view.View} and this alike to implement
+ */
+public interface OnScrollChangedListener {
+    void onScrollChanged(Object who, int l, int t, int oldl, int oldt);
+}
