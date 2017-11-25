8e5da1e4acaec208ff7d759e0739cf6c003becdd
Walid <walid.turki@esprit.tn>
2017-11-25 23:45:03 +0100

    sharefragment edited : getiing user proilePictureUrl

--- app/src/main/java/com/androidprojects/esprit/ikotlin/fragments/ShareFragment.java
+++ app/src/main/java/com/androidprojects/esprit/ikotlin/fragments/ShareFragment.java
@@ -1,5 +1,6 @@
 package com.androidprojects.esprit.ikotlin.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -13,26 +14,34 @@ import android.widget.TextView;
 import com.androidprojects.esprit.ikotlin.R;
 import com.androidprojects.esprit.ikotlin.activities.SignupActivity;
 import com.androidprojects.esprit.ikotlin.adapters.ShareList_Adapter;
+import com.androidprojects.esprit.ikotlin.handlers.DataBaseHandler;
 import com.androidprojects.esprit.ikotlin.models.ForumPost;
+import com.androidprojects.esprit.ikotlin.models.User;
 
 
 public class ShareFragment extends Fragment {
 
 
     ShareList_Adapter adapter;
-    // static data for test
-    // to be filled from database
-    ForumPost[] posts =new ForumPost[]{
-             new ForumPost("Amal Hichri", SignupActivity.user.getPictureURL(),"How to build android app in 1 month",3,7,100),
-            new ForumPost("Walid Turki",SignupActivity.user.getPictureURL(),"A9ra mobile !! :D",7,3,200),
-             new ForumPost("Test user", SignupActivity.user.getPictureURL(),"Behi w mba3ed ??",6,1,800)
-    };
+    User user;
+    ForumPost[] posts;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_share, container, false);
+
+        /**get current user**/
+        user= DataBaseHandler.getInstance(getContext()).getUser();
+        // static data for test
+        // to be filled from database
+        posts =new ForumPost[]{
+
+                new ForumPost("Amal Hichri", user.getPictureURL(),"How to build android app in 1 month",3,7,100),
+                new ForumPost("Walid Turki",user.getPictureURL(),"A9ra mobile !! :D",7,3,200),
+                new ForumPost("Test user",user.getPictureURL(),"Behi w mba3ed ??",6,1,800)
+        };
 
         adapter=new ShareList_Adapter(getContext(),posts);
         ((ListView)v.findViewById(R.id.shareLv)).setAdapter(adapter);
@@ -59,6 +68,9 @@ public class ShareFragment extends Fragment {
                 // TODO Auto-generated method stub
             }
         });
+
+
+
         return v;
     }
 
